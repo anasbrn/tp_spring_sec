6320fa3117c1da0a6738ad498c11c105a374a934
BARNOCH Anas <anasbarnoch@gmail.com>
2025-06-23 22:28:29 +0100

    Store product & add validation

--- src/main/java/org/tp_spring_sec/controllers/ProductController.java
+++ src/main/java/org/tp_spring_sec/controllers/ProductController.java
@@ -1,7 +1,9 @@
 package org.tp_spring_sec.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +30,19 @@ public class ProductController {
     @PostMapping("/admin/products/delete/{id}")
     public String delete(@PathVariable(name = "id") Long id) {
         productRepository.deleteById(id);
+        return "redirect:/public/products";
+    }
+
+    @GetMapping("admin/products/new")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "views/product/new_product";
+    }
+
+    @PostMapping("admin/products/store")
+    public String store(@Valid Product product, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) return "views/product/new_product";
+        productRepository.save(product);
         return "redirect:/public/products";
     }
 }
